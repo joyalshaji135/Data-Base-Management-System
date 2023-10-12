2b5d2380606cd7fff68c632f3c86893773a6fa47
joyalshaji135 <joyalshaji135@gmail.com>
2023-10-12 23:29:10 +0530

    Create Database,Show,Drop,,Rename

--- Sql_Query/01-Query.sql
+++ Sql_Query/01-Query.sql
@@ -1,6 +1,23 @@
-create database name;
-show databases;
-use name;
+-- Create the Datadase
+
+create database database1;
+
+-- Use the Database  
+
+use database1;
+
+-- Show it the Databases 
 
 show databases;
 
+create database student;
+
+-- Rename the Database
+ 
+-- rename database student to teacher; 
+
+-- ALTER DATABASE student MODIFY NAME = teacher;
+
+
+
+    
